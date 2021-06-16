600bdd65d47e7f981c6a64525cb27deba59f0865
William <William.Barker98@hotmail.com>
2021-06-16 16:10:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+y = np.dot(X, np.array([1, 2])) + 3 # 6, 8, 9, 11
+
+print(y)
+reg = LinearRegression().fit(X, y)
+print(reg.score(X, y))
+
